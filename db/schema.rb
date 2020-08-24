93ab57bfd62e8ccd6613cdcced27f1a3f5f687b2
Stephen McAleese <stephenmcaleese11@gmail.com>
2020-08-24 17:34:34 +0100

    changed from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_14_113201) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "add_user_id_to_comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
